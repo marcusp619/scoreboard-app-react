fda6a10256e6cdeb6d74592c5ea2266be4181e52
marcusp619 <marcusp619@gmail.com>
2017-04-04 21:10:48 -0600

    Initial commit
    Cleaning up files i didnt need

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from React</h1>
+        <p>I was rendered from the App Component</p>
+      </div>
+    );
+  }
+}
+
+export default App;
